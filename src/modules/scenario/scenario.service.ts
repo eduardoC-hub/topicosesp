892a56f0c3f21b2de5327d5f0b6bb614c1c70eb4
eduardoC-hub <eduardomarquiori99@gmail.com>
2025-09-15 08:52:29 -0400

    finalizado o crud

--- src/modules/scenario/scenario.service.ts
+++ src/modules/scenario/scenario.service.ts
@@ -1,37 +1,37 @@
 /* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
 import { CreateScenarioDto } from './dto/create-scenario.dto';
-import { UpdateScenarioDto } from './dto/update-scenario.dto';
 import { CreateScenarioUseCase } from './use-cases/create-scenario.use-case';
-import { ListScenarioRepository } from './repository/list-scenario-repository';
-import { findOneScenarioRepository } from './repository/findone.scenario.repository';
-import { DeleteScenarioUseCase } from './use-cases';
+import { DeleteScenarioUseCase, findOneScenarioUseCase, ListScenarioUseCase } from './use-cases';
+import { UpdateScenarioUseCase } from './use-cases/update-scenario.use-case';
+import { UpdateScenarioDto } from './dto/update-scenario.dto';
 
 @Injectable()
 export class ScenarioService {
   constructor(private readonly createScenarioUseCase: CreateScenarioUseCase, 
-    private readonly listScenarioUseCase: ListScenarioRepository,
-    private readonly findoneScenarioUseCase: findOneScenarioRepository,
-  private readonly DeleteScenarioUseCase: DeleteScenarioUseCase) {}
+    private readonly listScenarioUseCase: ListScenarioUseCase,
+    private readonly findoneScenarioUseCase: findOneScenarioUseCase,
+    private readonly deleteScenarioUseCase: DeleteScenarioUseCase,
+    private readonly updateScenarioUseCase: UpdateScenarioUseCase) {}
 
   create(data: CreateScenarioDto) {
     return this.createScenarioUseCase.execute(data);
   }
 
   findAll() {
-   return this.listScenarioUseCase.list();
+   return this.listScenarioUseCase.execute();
   }
 
 
   findOne(id: string) {
-    return this.findoneScenarioUseCase.findOne(id);
+    return this.findoneScenarioUseCase.execute(id);
   }
 
-  update(id: number, updateScenarioDto: UpdateScenarioDto) {
-    return `This action updates a #${id} scenario`;
+  update(id: string,data:UpdateScenarioDto) {
+    return this.updateScenarioUseCase.execute(id, data);
   }
 
   remove(id:string) {
-    return this.DeleteScenarioUseCase.execute(id);
+    return this.deleteScenarioUseCase.execute(id);
   }
 }
--- src/modules/scenario/use-cases/index.ts
+++ src/modules/scenario/use-cases/index.ts
@@ -3,3 +3,4 @@ export * from './create-scenario.use-case';
 export * from './list-scenario-useCases';
 export * from './findone.scenario.use-cases';
 export * from './delete.scenario.use-cases';
+export * from './update-scenario.use-case';
